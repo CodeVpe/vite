3b7ccab4416258caea0023541e61cf157e9170f3
arthur.liu <arthur.liu@g-town.com.cn>
2021-07-21 17:02:34 +0800

    init

---
+++ src/views/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+import axios from '../utils/axios'
+
+export default defineComponent({
+  setup() {
+    axios
+      .get('/user/XPoet')
+      .then((res) => {
+        console.log('res:', res)
+      })
+      .catch((err) => {
+        console.log('err:', err)
+      })
+  }
+})
+</script>
+<style lang="sass" scoped></style>
