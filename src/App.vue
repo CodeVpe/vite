903973df04085a583068a07fd733c97f812ee977
arthur.liu <arthur.liu@g-town.com.cn>
2021-07-21 17:09:30 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+  </div>
 </template>
 
 <script setup>
